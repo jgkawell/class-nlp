d5043c6ab8bfc2fdd2fbaf2f32a862a365eb2136
Jack Kawell <jgkawell@outlook.com>
2018-11-26 16:57:23 -0700

    Almost finished prelim work on hw-3

--- hw-3/kawell-jack-assgn3.py
+++ hw-3/kawell-jack-assgn3.py
@@ -1,45 +1,56 @@
 from collections import Counter
 import numpy as np
 import random
+import re
 
 # initialize the data field variables
 _pos_train_data = []
 _neg_train_data = []
-_state_space = []
 _observation_space = []
-_initial_prob = []
-_observation_prob = [[]]
-_transition_prob = [[]]
 
 # global string values for files and markers
 _pos_file_name = "hotelPosT-train.txt"
+_pos_train_name = "pos-train.txt"
+_pos_dev_name = "pos-dev.txt"
+
 _neg_file_name = "hotelNegT-train.txt"
-_train_file_name = "train.txt"
-_dev_file_name = "dev.txt"
+_neg_train_name = "neg-train.txt"
+_neg_dev_name = "neg-dev.txt"
+
+
 _test_file_name = "test.txt"
-_dev_results_file_name = "dev-results.txt"
+_pos_dev_results_name = "pos-dev-results.txt"
+_neg_dev_results_name = "neg-dev-results.txt"
+
 _test_results_file_name = "test-results.txt"
 _unknown_word_marker = "<UNK>"
-_sentence_marker = "<s>"
 
 # global int values for algorithm params
 _dev_partition_ratio = 10
         
 # preprocess the data to create a training and dev set
 def preprocess(run):
+    global _pos_train_data
+    global _neg_train_data
 
     file_name = None
+    train_name = None
+    dev_name = None
     if run == "pos":
         file_name = _pos_file_name
+        train_name = _pos_train_name
+        dev_name = _pos_dev_name
     elif run == "neg":
         file_name = _neg_file_name
+        train_name = _neg_train_name
+        dev_name = _neg_dev_name
 
     # read in the training data
     cur_file = open(file_name, "r")
 
     # create subset files for training and dev
-    train_file = open(_train_file_name, "w")
-    dev_file = open(_dev_file_name, "w")
+    train_file = open(train_name, "w")
+    dev_file = open(dev_name, "w")
 
     train_lines = []
     for line in cur_file:
@@ -70,61 +81,63 @@ def preprocess(run):
 
 # calculate all needed probs and counts for hmm/viterbi
 def train():
-    global _pos_train_data
-    global _neg_train_data
 
     # get the list of sentences with words and pos for training
-    pos_words, neg_words = getWords(_pos_train_data, _neg_train_data)
+    pos_dict = getReviews(_pos_train_data)
+    neg_dict = getReviews(_neg_train_data)
 
-    len_observation_space, len_state_space = buildSpaces()
+    buildObservationSpace(pos_dict)
+    buildObservationSpace(neg_dict)
 
     # iterate through training data and count the transitions for pos
-    transition_count_matrix, observation_count_matrix = buildCountMatrices(len_state_space, len_observation_space)
+    pos_counts_dict = buildCountDict(pos_dict)
+    neg_counts_dict = buildCountDict(neg_dict)
+
 
     # find the observation likelihood for the pos given words
-    _observation_prob = buildProbMatrix(len_state_space, len_observation_space, observation_count_matrix)
+    pos_prob, neg_prob = buildProbDicts(pos_counts_dict, neg_counts_dict)
 
-    # find the transition probability for pos given previous pos
-    _transition_prob = buildProbMatrix(len_state_space, len_state_space, transition_count_matrix)
+    print("Finished training...")
 
-    # calculate the initial probabilities
-    sentence_beginning_index = _state_space.index(_sentence_marker)
-    for s in range(0, len(_state_space)):
-        _initial_prob.append(_transition_prob[sentence_beginning_index][s])
-
-# pulls out words
-def getWords(_pos_train_data, _neg_train_data):
+# pulls out reviews into a dict of IDs and list of words
+def getReviews(data_lines):
     
-    for i in range(0, 2):
+    review_dict = dict()
+    for line in data_lines:
+        word_list = []
+        word = ""
+        id_num = ""
+        for char in line:
+            if len(id_num) == 0:
+                if char == "\t":
+                    id_num = word
+                    word = ""
+                else:
+                    word += char
 
-        data_lines = None
-        if i == 0:
-            data_lines = _pos_train_data
-        elif i == 1:
-            data_lines = _neg_train_data
+            else:
+                # finished word
+                if re.match(r"[\s\.\?\,\!]", char):
+                    # not word
+                    if word != "":
+                        word_list.append(word.lower())
+                        word = ""
+                else:
+                    word += char
 
-        data_words = [[]]
-        for line in data_lines:
-            for word in line:
-                print(word)
+        review_dict[id_num] = word_list
 
-
-    return 0, 0
+    return review_dict
 
 # scan through data and build the observation and state spaces
-def buildSpaces():
+def buildObservationSpace(train_data):
     global _observation_space
-    global _state_space
     
     # scan through data and find the spaces along with the single counts in the observation space
     _observation_space = []
     single_counts = []
-    _state_space = []
-    for sentence in _train_data:
-        for entry in sentence:
-            # pull out the word and pos
-            word = entry[1]
-            part = entry[2]
+    for word_list in train_data.values():
+        for word in word_list:
 
             # if word doesn't exist yet in observation space, add it
             # else, try to remove it from the single counts list
@@ -138,10 +151,6 @@ def buildSpaces():
                 except:
                     pass
 
-            # if pos doesn't exist yet in state space, add it
-            if part not in _state_space:
-                _state_space.append(part)
-
     # remove words with only a single count and replace them with <UNK>
     for word in single_counts:
         _observation_space.remove(word)
@@ -149,56 +158,42 @@ def buildSpaces():
     # add unknown word marker <UNK>
     _observation_space.append(_unknown_word_marker)
 
-    #  add sentence marker (<s>)
-    _state_space.append(_sentence_marker)
-
-    return (len(_observation_space), len(_state_space))
-
 # build the count matrices needed to build the prob matrices
-def buildCountMatrices(len_state_space, len_observation_space):
+def buildCountDict(train_data):
 
     # initialize as ones for laplace smoothing
-    transition_count_matrix = np.ones((len_state_space, len_state_space))
-    emission_count_matrix = np.ones((len_state_space, len_observation_space))
+    count_dict = dict()
+    for word in _observation_space:
+        count_dict[word] = 1
     
     # iterate through training data and count the transitions for pos and emissions for words
-    for sentence in _train_data:
-        prev_part = _sentence_marker
-        for entry in sentence:
-            cur_word = entry[1]
-            cur_part = entry[2]
-
-            # increment the count for the transition count
-            transition_count_matrix[_state_space.index(cur_part)][_state_space.index(prev_part)] += 1
-
-            # set the previous part
-            prev_part = cur_part
+    for word_list in train_data.values():
+        for word in word_list:
 
             # increment the count for the emission count
             try:
-               emission_count_matrix[_state_space.index(cur_part)][_observation_space.index(cur_word)] += 1
+                count_dict[word] += 1
             except:
-               emission_count_matrix[_state_space.index(cur_part)][_observation_space.index(_unknown_word_marker)] += 1
+                count_dict[_unknown_word_marker] += 1
 
-    return (transition_count_matrix, emission_count_matrix)
-
+    return count_dict
+    
 #  build the prob matrices (both transition and emission)
-def buildProbMatrix(num_rows, num_cols, count_matrix):
+def buildProbDicts(pos_counts_dict, neg_counts_dict):
 
-    # scan through and find the sums of the counts on each row (needed for laplace smoothing)
-    prob_matrix = np.zeros((num_rows, num_cols))
-    row_sums = np.zeros(num_rows)
-    for row in range(0, num_rows):
-        for col in range(0, num_cols): 
-            row_sums[row] += count_matrix[row][col]
+    word_sums = dict()
+    for word in _observation_space:
+        word_sums[word] = pos_counts_dict[word] + neg_counts_dict[word]
 
-    # find the transition probabilities for the pos
-    for row in range(0, num_rows):
-        for col in range(0, num_cols):
-            # add num_rows to denominator for laplace smoothing
-            prob_matrix[row][col] = count_matrix[row][col] / (row_sums[row] + num_rows)
+    pos_prob_dict = dict()
+    for word in _observation_space:
+        pos_prob_dict[word] = pos_counts_dict[word] / word_sums[word]
 
-    return prob_matrix
+    neg_prob_dict = dict()
+    for word in _observation_space:
+        neg_prob_dict[word] = neg_counts_dict[word] / word_sums[word]
+
+    return pos_prob_dict, neg_prob_dict
 
 #  test using the basic "most frequent tag" technique
 def test(run_file_name, results_file_name):
@@ -207,18 +202,18 @@ def test(run_file_name, results_file_name):
     results_file = open(results_file_name, "w")
 
     # retrieve data to run through model
-    data = getWords(run_file_name, dev=False)
+    data = getReviews(run_file_name)
             
     # go through data and run viterbi on each sentence, printing the results
-    for sentence in data:
+    for key, word_list in data:
         observations = []
-        for entry in sentence:
-            word = entry[1]
+        for word in word_list:
             # try to get the index of the word, if not found, substitute the unknown word marker
             try:
-                observations.append(_observation_space.index(word))
+                check = _observation_space.index(word)
+                observations.append(word)
             except:
-                observations.append(_observation_space.index(_unknown_word_marker))
+                observations.append(_unknown_word_marker)
 
         # run viterbi on each sentence
         best_path = viterbi(len(_state_space), _transition_prob, _observation_prob, _initial_prob, observations)
@@ -289,7 +284,7 @@ if  __name__ == "__main__":
 
     # test on the dev set
     print("Running dev data...")
-    test(_dev_file_name, _dev_results_file_name)
+    test(_pos_dev_name, _pos_dev_results_name)
 
     # test on the test set
     print("Running test data...")
